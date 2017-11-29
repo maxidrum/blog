be7fb01570a8dc15e30cd129eed315578e0e1b46
maxidrum <maxksidrum@gmail.com>
2017-11-29 03:14:27 +0200

    update view create

--- application/models.py
+++ application/models.py
@@ -7,14 +7,14 @@ ROLE_ADMIN = 1
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(140))
-    # body = db.Column(db)
-    # created = db.Column(db.DateTime, default=datetime.now())
+    body = db.Column(db.Text)
+    created = db.Column(db.DateTime, default=datetime.now())
 
     def __init__(self, *args, **kwargs):
         super(Post, self).__init__(*args, **kwargs)
 
     def __repr__(self):
-        return
+        return "<Post: title: {}".format(self.title)
 
 
 class User(db.Model):
--- application/view.py
+++ application/view.py
@@ -1,12 +1,17 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
 from forms import PostFrom
 from models import Post
-from application import app
+from application import app, db
 
-@app.route("/create")
+@app.route("/create", methods=['GET', 'POST'])
 def create_post():
     form = PostFrom()
+    if request.method == 'POST':
+        post = Post(title=form.data['title'], body=form.data['body']) #TODO: ref
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for("main"))
     return render_template("create_post.html", form=form)
 
 
