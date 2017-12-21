dcbed8527ddfa5770017485e80ab560559c97ab8
maxidrum <maxksidrum@gmail.com>
2017-12-21 23:55:35 +0200

    small update for login

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 import application.view
--- application/view.py
+++ application/view.py
@@ -1,8 +1,11 @@
 from flask import render_template, request, redirect, url_for
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import login_user, logout_user
 
 from forms import PostFrom, LoginForm
 from models import Post
-from application import app, db
+from application import app, db, login_manager
+
 
 @app.route("/create", methods=['GET', 'POST'])
 def create_post():
@@ -22,7 +25,14 @@ def main():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
+    form = LoginForm(request.form)
     if request.method == 'POST' and form.validate_on_submit():
-        # user = User.query.filter_by(username=form.data['email']).first()
+        return redirect(url_for('main'))
+        # user = User.query.filter_by(username=form['email']).first()
+    # check_password_hash(user.password, form['password'])
+    # login_user(user)
     return render_template("login.html", form=form)
+
+@app.route('/logout')
+def logout():
+    return "Logged out"
